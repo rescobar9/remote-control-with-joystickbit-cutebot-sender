0d5b81d4ef9472000c3022ba8526afc0cf993909
Rudy Escobar <67067784+rescobar9@users.noreply.github.com>
2023-01-15 21:38:49 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
-let y = 0
-let x = 0
 radio.setGroup(1)
 basic.forever(function () {
-    x = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, -100, 100)
-    y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 1023, 0, -100, 100)
-    radio.sendValue("x", x)
-    radio.sendValue("y", y)
+    if (GHBit.Rocker(GHBit.enRocker.Up)) {
+        radio.sendString("F")
+    } else if (GHBit.Rocker(GHBit.enRocker.Down)) {
+        radio.sendString("B")
+    } else if (GHBit.Rocker(GHBit.enRocker.Right)) {
+        radio.sendString("R")
+    } else if (GHBit.Rocker(GHBit.enRocker.Left)) {
+        radio.sendString("L")
+    } else if (GHBit.Rocker(GHBit.enRocker.Nostate)) {
+        radio.sendString("S")
+    }
 })
